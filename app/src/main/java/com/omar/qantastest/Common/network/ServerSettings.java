73a8a3be3ffa23c23d27573a64b4bc8f85eece2c
Omar Mujtaba <omar.mujtaba@gmail.com>
2018-05-23 22:01:03 +1000

    Import network calls code and library dependencies

--- app/src/main/java/com/omar/qantastest/Common/network/ServerSettings.java
+++ app/src/main/java/com/omar/qantastest/Common/network/ServerSettings.java
@@ -1,7 +1,17 @@
 package com.omar.qantastest.Common.network;
 
+import okhttp3.logging.HttpLoggingInterceptor;
+
 /**
  * Created by omz on 23/5/18
  */
 public class ServerSettings {
+
+    public static class Domain {
+        public static final long OKHTTP_CLIENT_READ_TIMEOUT_SECONDS = 6;
+        public static final long OKHTTP_CLIENT_CONNECT_TIMEOUT_SECONDS = 6;
+        public static final HttpLoggingInterceptor.Level HTTP_LOG_LEVEL = HttpLoggingInterceptor.Level.BODY;
+
+        public static final String BASE_URL = "https://g525204.github.io/";
+    }
 }
